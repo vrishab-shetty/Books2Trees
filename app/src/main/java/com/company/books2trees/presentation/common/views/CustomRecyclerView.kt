f751e1882288a27c9a32468ab86f3747c22ae3d9
vrishab <vrishabshetty@gmail.com>
2025-09-08 22:09:21 -0400

    refactor(view): Simply adaptive grid implementation

--- app/src/main/java/com/company/books2trees/presentation/common/views/CustomRecyclerView.kt
+++ app/src/main/java/com/company/books2trees/presentation/common/views/CustomRecyclerView.kt
@@ -3,128 +3,15 @@ package com.company.books2trees.presentation.common.views
 import android.R
 import android.content.Context
 import android.util.AttributeSet
-import android.view.View
+import androidx.core.content.withStyledAttributes
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlin.math.abs
-import androidx.core.content.withStyledAttributes
 
-class CustomGridLayoutManager(val context: Context, _spanCount: Int) :
-    GridLayoutManager(context, _spanCount) {
-
-    /* Called when searching for a focusable view in the
-     given direction has failed for the current
-     content of the RecyclerView.*/
-    override fun onFocusSearchFailed(
-        focused: View,
-        focusDirection: Int,
-        recycler: RecyclerView.Recycler,
-        state: RecyclerView.State
-    ): View? {
-        return try {
-            val fromPos = getPosition(focused)
-            val nextPos = getNextViewPos(fromPos, focusDirection)
-            findViewByPosition(nextPos)
-        } catch (e: Exception) {
-            null
-        }
-    }
-
-    /* This method gives a LayoutManager an opportunity to
-    intercept the initial focus search before the default
-    behavior of FocusFinder is used.*/
-    override fun onInterceptFocusSearch(focused: View, direction: Int): View? {
-        return try {
-            val fromPos = getPosition(focused)
-            val nextPos = getNextViewPos(fromPos, direction)
-            findViewByPosition(nextPos)
-        } catch (e: Exception) {
-            null
-        }
-    }
-
-
-    private fun getNextViewPos(fromPos: Int, direction: Int): Int {
-        val offset = calcOffsetToNextView(direction)
-
-        if (hitBorder(fromPos, offset)) {
-            return fromPos
-        }
-
-        return fromPos + offset
-    }
-
-    private fun calcOffsetToNextView(direction: Int): Int {
-        val spanCount = this.spanCount
-        val orientation = this.orientation
-
-        // fixes arabic by inverting left and right layout focus
-        val correctDirection = if (this.isLayoutRTL) {
-            when (direction) {
-                View.FOCUS_RIGHT -> View.FOCUS_LEFT
-                View.FOCUS_LEFT -> View.FOCUS_RIGHT
-                else -> direction
-            }
-        } else direction
-
-        if (orientation == VERTICAL) {
-            when (correctDirection) {
-                View.FOCUS_DOWN -> {
-                    return spanCount
-                }
-
-                View.FOCUS_UP -> {
-                    return -spanCount
-                }
-
-                View.FOCUS_RIGHT -> {
-                    return 1
-                }
-
-                View.FOCUS_LEFT -> {
-                    return -1
-                }
-            }
-        } else if (orientation == HORIZONTAL) {
-            when (correctDirection) {
-                View.FOCUS_DOWN -> {
-                    return 1
-                }
-
-                View.FOCUS_UP -> {
-                    return -1
-                }
-
-                View.FOCUS_RIGHT -> {
-                    return spanCount
-                }
-
-                View.FOCUS_LEFT -> {
-                    return -spanCount
-                }
-            }
-        }
-        return 0
-    }
-
-    private fun hitBorder(from: Int, offset: Int): Boolean {
-        val spanCount = spanCount
-
-        return if (abs(offset) == 1) {
-            val spanIndex = from % spanCount
-            val newSpanIndex = spanIndex + offset
-            newSpanIndex < 0 || newSpanIndex >= spanCount
-        } else {
-            val newPos = from + offset
-            newPos in spanCount..-1
-        }
-    }
-}
 
 class AutoFitRecyclerView @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) :
     RecyclerView(context, attrs) {
 
-    val manager = CustomGridLayoutManager(context, 2) // THIS CONTROLS SPANS
+    val manager = GridLayoutManager(context, 2) // THIS CONTROLS SPANS
 
     private var columnWidth = -1
 
