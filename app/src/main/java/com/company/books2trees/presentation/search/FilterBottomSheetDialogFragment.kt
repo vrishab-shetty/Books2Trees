67ce2ea308e1c0df118f641625012064b19c1e6a
vrishab <vrishabshetty@gmail.com>
2025-09-10 20:28:46 -0400

    refactor(base): Create ViewBindingBottomSheetDialogFragment
    Introduces a new abstract class, to encapsulate the boilerplate logic for setting up and tearing down ViewBinding within BottomSheetDialogFragment

--- app/src/main/java/com/company/books2trees/presentation/search/FilterBottomSheetDialogFragment.kt
+++ app/src/main/java/com/company/books2trees/presentation/search/FilterBottomSheetDialogFragment.kt
@@ -1,9 +1,7 @@
 package com.company.books2trees.presentation.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.lifecycle.Lifecycle
@@ -11,25 +9,15 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.company.books2trees.R
 import com.company.books2trees.databinding.SelectListPageBinding
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.company.books2trees.presentation.common.base.ViewBindingBottomSheetDialogFragment
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
-class FilterBottomSheetDialogFragment : BottomSheetDialogFragment() {
-
-    private var _binding: SelectListPageBinding? = null
-    private val binding get() = _binding!!
+class FilterBottomSheetDialogFragment : ViewBindingBottomSheetDialogFragment<SelectListPageBinding>(
+    SelectListPageBinding::inflate
+) {
 
     private val vm: SearchViewModel by sharedViewModel()
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = SelectListPageBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -40,33 +28,36 @@ class FilterBottomSheetDialogFragment : BottomSheetDialogFragment() {
         observeSelectedFilter()
     }
 
-    private fun setupCancelButton() {
+    private fun setupCancelButton() = useBinding { binding ->
         binding.cancelBtn.setOnClickListener {
             dismiss() // Close the bottom sheet
         }
     }
 
-    private fun setupApplyButton() {
+    private fun setupApplyButton() = useBinding { binding ->
         binding.applyBtn.setOnClickListener {
             vm.applyFilter()
             dismiss() // Close the bottom sheet
         }
     }
 
-    private fun setupListView() {
+    private fun setupListView() = {
         val genreAdapter = ArrayAdapter(
             requireContext(),
             R.layout.filter_single_choice, // Use Android's built-in layout
             vm.genreList
         )
 
-        binding.listview.apply {
-            adapter = genreAdapter
-            choiceMode = ListView.CHOICE_MODE_SINGLE
-            setOnItemClickListener { _, _, position, _ ->
-                vm.onFilterItemClicked(position)
+        useBinding { binding ->
+            binding.listview.apply {
+                adapter = genreAdapter
+                choiceMode = ListView.CHOICE_MODE_SINGLE
+                setOnItemClickListener { _, _, position, _ ->
+                    vm.onFilterItemClicked(position)
+                }
             }
         }
+
     }
 
     private fun observeSelectedFilter() {
@@ -77,15 +68,10 @@ class FilterBottomSheetDialogFragment : BottomSheetDialogFragment() {
                     val position = vm.genreList.indexOf(currentFilter)
                     if (position != -1) {
                         // Update the ListView's selection
-                        binding.listview.setItemChecked(position, true)
+                        useBinding { binding -> binding.listview.setItemChecked(position, true) }
                     }
                 }
             }
         }
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null // Avoid memory leaks
     }
 }
